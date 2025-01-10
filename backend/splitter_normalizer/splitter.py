93e0d1819599341b7fbb0b0f0ba667f6cfa07226
Eduard <e.sabo@student.rug.nl>
2025-01-11 00:51:34 +0100

    Modified Splitter to handle 2 RabbitMQ queues

--- backend/splitter_normalizer/splitter.py
+++ backend/splitter_normalizer/splitter.py
@@ -5,11 +5,11 @@ import pika
 class Splitter:
 
     # Initialize RabbitMQ queue
-    def __init__(self, rabbitmq_host='localhost', queue_name='supermarket_queue'):
-        self.queue_name = queue_name
+    def __init__(self, rabbitmq_host='localhost'):
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host))
         self.channel = self.connection.channel()
-        self.channel.queue_declare(queue=self.queue_name)
+        self.channel.queue_declare(queue='ah_queue')
+        self.channel.queue_declare(queue='jumbo_queue')
 
     # read items from json file and split the message into individual messages
     def preprocess_message(self, input_file):
@@ -25,69 +25,30 @@ class Splitter:
         return parsed_messages
 
     # add the message into a RabbitMQ queue
-    def enque_message(self, parsed_messages):
+    def enque_message(self, parsed_messages, queue_name):
         for message in parsed_messages:
             # Publish each message to the RabbitMQ queue
             self.channel.basic_publish(
                 exchange='',
-                routing_key=self.queue_name,
+                routing_key = queue_name,
                 body=json.dumps(message)
             )
             print(f"Message enqueued: {message}")
-
-    # consume messages from RabbitMQ queue
-    def process_queue(self):
-        # Consume messages from the RabbitMQ queue
-        def callback(ch, method, properties, body):
-            message = json.loads(body)
-            print(f"Processing message: {message}")
-            ch.basic_ack(delivery_tag=method.delivery_tag)
-
-        self.channel.basic_consume(queue=self.queue_name, on_message_callback=callback, auto_ack=False)
-        print("Waiting for messages. To exit, press CTRL+C")
-        self.channel.start_consuming()
 
     def close_connection(self):
         self.connection.close()
 
 
-# Example splitter for ah
-# if __name__ == "__main__":
-#     splitter = Splitter(rabbitmq_host='localhost', queue_name='ah_queue')
+# Example use case
+if __name__ == "__main__":
+    splitter = Splitter()
 
-#     ah_file = 'savedata_AH.json'
+    # Process AH data
+    ah_data = splitter.preprocess_message('savedata_AH.json')
+    splitter.enque_message(ah_data, 'ah_queue')
 
-#     parsed_messages_ah = splitter.preprocess_message(ah_file)
-#     if parsed_messages_ah:
-#         splitter.enque_message(parsed_messages_ah)
-#         #splitter.process_queue()
+    # Process Jumbo data
+    jumbo_data = splitter.preprocess_message('savedata_jumbo.json')
+    splitter.enque_message(jumbo_data, 'jumbo_queue')
 
-#     splitter.close_connection()
-
-
-
-
-
-
-# Test Splitter below
-# if __name__ == "__main__":
-
-#     ah_file = 'savedata_AH.json'
-#     jumbo_file = 'savedata_jumbo.json'
-
-#     splitter = Splitter()
-
-#     parsed_messages_ah = splitter.preprocess_message(ah_file)
-#     parsed_messages_jumbo = splitter.preprocess_message(jumbo_file)
-    
-#     if parsed_messages_ah and parsed_messages_jumbo:
-#         ah_queue = queue.Queue()
-#         jumbo_queue = queue.Queue()
-
-#         splitter.enque_message(parsed_messages_ah, ah_queue)
-#         splitter.enque_message(parsed_messages_jumbo,jumbo_queue)
-#         print(f"ah nr items in queue: {ah_queue.qsize()}")
-#         print(f"jumbo nr items in queue: {jumbo_queue.qsize()}")
-
-#         # splitter.process_queue(ah_queue)
-#         # splitter.process_queue(jumbo_queue)
+    splitter.close_connection()
