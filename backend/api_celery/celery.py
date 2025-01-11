b3ae7f997955f57fb7baa1f4b5d00c7ecb86c5ac
Himawan Indra Bayu <h.i.bayu@student.rug.nl>
2025-01-11 15:27:09 +0100

    commit

--- backend/api_celery/celery.py
+++ backend/api_celery/celery.py
@@ -10,17 +10,19 @@ app.config_from_object('api_celery.celeryconfig')
 app.conf.beat_schedule = {
     "schedule_fetch_AH": {
         "task": "api_celery.tasks.fetch_albert_heijn",
-        # "schedule": 300.0
+        # "schedule": 30.0
+        "schedule": crontab(minute="*/10")
+    },
+    "schedule_fetch_jumbo": {
+        "task": "api_celery.tasks.fetch_jumbo",
+        # "schedule": 30.0
         "schedule": crontab(minute="*/5")
     },
-    # "schedule_fetch_jumbo": {
-    #     "task": "api_celery.tasks.fetch_jumbo",
-    #     # "schedule": 300.0
-    #     "schedule": crontab(minute="*/5")
-    # },
 }
 
 # celery -A api_celery worker --loglevel=info --pool=solo
 # celery -A api_celery worker --loglevel=info --pool=solo --concurrency=2
+# celery worker --loglevel=info --pool=solo --concurrency=2
 # celery -A api_celery beat --loglevel=info
+# celery beat --loglevel=info
 # Celery -P threads or --pool=solo options solves the issue.
